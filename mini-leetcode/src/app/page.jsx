04ca8989bc73a1ac5e15d1c0a1ecbff7fecbf1c6
“Anushka <anushka612004@gmail.com>
2026-01-20 11:58:07 +0530

    page and layout

---
+++ mini-leetcode/src/app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">
+        Mini LeetCode with AI
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Built using Next.js and Supabase
+      </p>
+    </main>
+  );
+}
