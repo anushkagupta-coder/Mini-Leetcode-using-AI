f486577bddc91001791f1765a5794f8278761422
“Anushka <anushka.kufam22@sinhgad.edu>
2026-01-22 21:56:32 +0530

    auth

--- mini-leetcode/src/app/layout.jsx
+++ mini-leetcode/src/app/layout.jsx
@@ -1,16 +1,19 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main className="min-h-screen">
-          {children}
-        </main>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
